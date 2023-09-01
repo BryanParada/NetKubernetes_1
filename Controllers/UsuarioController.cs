8c9a2992b9e3af9fc6d00eb223146ec74b116c29
Bryan Parada <bryan.parada.cea@gmail.com>
2023-09-01 16:51:27 -0400

    E31 - Controllers .NET

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using NetKubernetes.Data.Usuarios;
+using NetKubernetes.Dtos.UsuarioDtos;
+
+namespace NetKubernetes.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsuarioController : ControllerBase{
+
+    private readonly IUsuarioRepository _repository;
+
+    public UsuarioController(IUsuarioRepository respository)
+    {
+        _repository = respository;
+    }
+
+    [AllowAnonymous] //puede ser consultado desde cualquier tipo de cliente
+    [HttpPost("/login")]
+    public async Task<ActionResult<UsuarioResponseDto>> Login(
+        [FromBody] UsuarioLoginRequestDto request
+    ){
+        return await _repository.Login(request);
+
+    }
+
+    [AllowAnonymous] //puede ser consultado desde cualquier tipo de cliente
+    [HttpPost("/registrar")]
+    public async Task<ActionResult<UsuarioResponseDto>> registrar(
+        [FromBody] UsuarioRegistroRequestDto request
+    ){
+        return await _repository.RegistroUsuario(request);
+
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<UsuarioResponseDto>> DevolverUsuario(){
+        return await _repository.GetUsuario();
+
+    }
+
+
+
+}
